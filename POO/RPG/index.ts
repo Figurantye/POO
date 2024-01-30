1343707ff9939b31850c7fad7a57b0d000e852b1
Gustavo Wazlawick <143718452+Figurantye@users.noreply.github.com>
2024-01-30 10:06:12 -0300

    Update index.ts

--- POO/RPG/index.ts
+++ POO/RPG/index.ts
@@ -4,6 +4,6 @@ import { Warrior } from "./Warrior";
 const theWarrior:Warrior = new Warrior("Arthur", "lancer", 35);
 const moonster:Monster = new Monster("Demonio");
 
-moonster.infoMonster()
-theWarrior.attackWarrior(moonster);
-moonster.infoMonster()
+moonster.info()
+theWarrior.attack(moonster);
+moonster.info()
