feccdfb2f51c5f79e35c254a724a5ef4f94d115d
Gustavo Wazlawick <143718452+Figurantye@users.noreply.github.com>
2024-01-30 09:49:49 -0300

    Update Warrior.js

--- POO/RPG/Warrior.js
+++ POO/RPG/Warrior.js
@@ -1,74 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Warrior = void 0;
-var Warrior = /** @class */ (function () {
-    function Warrior(name, type, strenght) {
-        this.name = name;
-        this.type = type;
-        this.strenght = strenght;
-        this.health = 100;
+import { Monster } from "./Monster"
+
+export class Warrior{
+    private name:string
+    private type:string
+    private strenght:number
+    private health:number
+
+    constructor(name:string, type:string, strenght:number){
+        this.name = name
+        this.type = type
+        this.strenght = strenght
+        this.health = 100
     }
-    Warrior.prototype.info = function () {
-        console.log("Warrior info \nname".concat(this.name, "; \nType: ").concat(this.type, " \nstrenght: ").concat(this.strenght, " \nhealth: ").concat(this.health));
-    };
-    Warrior.prototype.attack = function (moonster) {
-        var damage = this.strenght;
-        console.log("o poderoso ".concat(this.type, " de nome ").concat(this.name, " atacou o maldito ").concat(moonster.nameMonster, " e causou ").concat(damage, " de dano"));
-        moonster.reciveDamageMonster(damage);
-    };
-    Warrior.prototype.reciveDamage = function (damage) {
-        this.health -= damage;
-        console.log("o poderoso ".concat(this.name, " recebeu ").concat(damage, ", restando somente ").concat(this.health, " de saude"));
-    };
-    Object.defineProperty(Warrior.prototype, "nameWarrior", {
-        get: function () {
-            return this.name;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "typeWarrior", {
-        get: function () {
-            return this.type;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "strenghtWarrior", {
-        get: function () {
-            return this.strenght;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "healthWarrior", {
-        get: function () {
-            return this.health;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "infoWarrior", {
-        get: function () {
-            return this.info;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "attackWarrior", {
-        get: function () {
-            return this.attack;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Warrior.prototype, "reciveDamageWarrior", {
-        get: function () {
-            return this.reciveDamage;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Warrior;
-}());
-exports.Warrior = Warrior;
+    private info():void{
+        console.log(`Warrior info \nname${this.name}; \nType: ${this.type} \nstrenght: ${this.strenght} \nhealth: ${this.health}`);}
+
+    public attack(moonster:Monster):void{
+        const damage = this.strenght
+        console.log(`o poderoso ${this.type} de nome ${this.name} atacou o maldito ${moonster.getName} e causou ${damage} de dano`);
+        moonster.reciveDamage(damage)
+    }
+
+    public reciveDamage(damage:number):void{
+        this.health -= damage
+        console.log(`o poderoso ${this.name} recebeu ${damage}, restando somente ${this.health} de saude`);
+    }
+    public getName(){
+        return this.name
+    }
+}
