f73ea2d3d7bc56b4496056305f898d93c5b358f1
Gustavo Wazlawick <143718452+Figurantye@users.noreply.github.com>
2024-01-30 00:41:29 -0300

    Update Warrior.js

--- POO/RPG/Warrior.js
+++ POO/RPG/Warrior.js
@@ -13,13 +13,62 @@ var Warrior = /** @class */ (function () {
     };
     Warrior.prototype.attack = function (moonster) {
         var damage = this.strenght;
-        console.log("o poderoso ".concat(this.type, " de nome ").concat(this.name, " atacou o maldito ").concat(moonster.name, " e causou ").concat(damage, " de dano"));
-        moonster.reciveDamage(damage);
+        console.log("o poderoso ".concat(this.type, " de nome ").concat(this.name, " atacou o maldito ").concat(moonster.nameMonster, " e causou ").concat(damage, " de dano"));
+        moonster.reciveDamageMonster(damage);
     };
     Warrior.prototype.reciveDamage = function (damage) {
         this.health -= damage;
         console.log("o poderoso ".concat(this.name, " recebeu ").concat(damage, ", restando somente ").concat(this.health, " de saude"));
     };
+    Object.defineProperty(Warrior.prototype, "nameWarrior", {
+        get: function () {
+            return this.name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "typeWarrior", {
+        get: function () {
+            return this.type;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "strenghtWarrior", {
+        get: function () {
+            return this.strenght;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "healthWarrior", {
+        get: function () {
+            return this.health;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "infoWarrior", {
+        get: function () {
+            return this.info;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "attackWarrior", {
+        get: function () {
+            return this.attack;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Warrior.prototype, "reciveDamageWarrior", {
+        get: function () {
+            return this.reciveDamage;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Warrior;
 }());
 exports.Warrior = Warrior;
