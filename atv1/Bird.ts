8ec448b976f838e3b989e10e26aaf929c7248994
Figurantye <eva01.gustavo@gmail.com>
2024-02-05 09:15:08 -0300

    update bird

--- atv1/Bird.ts
+++ atv1/Bird.ts
@@ -1,13 +1,13 @@
 import { Animal } from "./Animal";
 export class Bird extends Animal{
     private wingspan:number
-
     constructor(name:string, weight:number){
         super(name, weight)
     }
 
-    fly():void{
-        console.log(`the ${this.getName} is flying`);
+    private fly():void{
+        console.log(`the ${this.getName} is flying`)
     }
-
 }
+const bird = new Bird(`Arara`, 0.5)
+console.log(bird.emitSound("piupiu"));
