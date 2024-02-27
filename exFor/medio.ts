df704f3c9c59c008f6cb53838d7ace816013455c
Figurantye <eva01.gustavo@gmail.com>
2024-02-27 11:47:52 -0300

    atualizando for

--- exFor/medio.ts
+++ exFor/medio.ts
@@ -1,16 +1,31 @@
-let primo:number[] = [5, 12, 8, 14, 3, 20, 31]
+/*
+//1
+let primo:number[] = [5, 12, 8, 14, 3, 20, 7]
 
 for(let i:number = 0; i < primo.length; i++){
-    if (primo[i] == 2){
-        console.log(`${primo[i]} é primo`);
-    } else if (primo[i] % 2 == 0){
-        console.log(`${primo[i]} não é primo`);
-    } else if (primo[i] == 3){
-        console.log(`${primo[i]} é primo`);
-    } else if (primo[i] % 3 == 0){
+    if(primo[i] % i == 0){
         console.log(`${primo[i]} não é primo`);
     } else {
-        console.log(`${primo[i]} é primo`);
+        console.log(`${primo[i]} é primo`);   
     }
 }
 
+//2
+let palavra:string[] = ['p', 'a', 'l', 'a', 'v', 'r', 'e', 'a', 'd', 'o',]
+
+for(let i:number = 0; i < palavra.length; i++){
+    if(palavra[i] !== 'a' || 'e' || 'i' || 'o' || 'u'){
+        palavra.pop()
+    }    
+}
+console.log(palavra.length);
+*/
+
+//3 Escreva um programa que calcule a média das notas armazenadas em um array. Cada item é uma nota obtida pelo aluno.
+let nota:number[] = [8, 7, 6, 8, 9]
+let soma = 0
+for(let i:number = 0; i <= nota.length; i++){
+    soma += nota[i];
+}
+
+console.log(soma / nota.length);
